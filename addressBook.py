b841cf150f5681be9a8bae15a713e2ae79ffdbb4
rednerrus <gregg.abbott@gmail.com>
2015-01-07 21:36:48 -0800

    added initial menu

---
+++ addressBook.py
@@ -0,0 +1,25 @@
+__author__ = 'root'
+test = True
+while test == True:
+    print "Welcome to Your Address Book"
+    print "Please choose one of the following:"
+    print "1. Create Contact"
+    print "2. Edit Contact"
+    print "3. View Contact"
+    print "4. Delete Contact"
+
+    selection1 = raw_input(">")
+
+    if selection1 == 1:
+        #Create Contact
+
+    if selection1 == 2:
+        #Edit Contact
+
+    if selection1 == 3:
+        #View Contact
+
+    if selection1 == 4:
+        #Delete Contact
+
+
