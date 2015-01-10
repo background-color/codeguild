c7ba03392b61830844953b10fb3a590cc91d59c7
rednerrus <gregg.abbott@gmail.com>
2015-01-09 16:07:29 -0800

    Creates a deck of cards using dictionary as data type.

--- fizzbuzz/DeckOfCards.py
+++ fizzbuzz/DeckOfCards.py
@@ -1,2 +1,30 @@
 __author__ = 'root'
 
+ranks = ['a', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k']
+suits = ['spades', 'clubs', 'hearts', 'diamonds']
+deck = {}
+card = 0
+color = 'black'
+for i in suits:
+    if i == 'spades' or i == 'clubs':
+        color = 'black'
+    else:
+        color = 'red'
+    for j in ranks:
+        card += 1
+        d = {card: {"suit": i,
+                    "rank": j,
+                    "color": color}}
+        deck.update(d)
+print deck
+
+
+
+
+
+# print deck
+
+
+
+
+
