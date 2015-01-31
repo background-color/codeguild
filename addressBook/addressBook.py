a9a9c8b6abc79289f3b2ff849f897d085d6de4af
rednerrus <gregg.abbott@gmail.com>
2015-01-30 22:07:15 -0800

    Want to make sure that all files are up to date before messing with my distros

--- addressBook/addressBook.py
+++ addressBook/addressBook.py
@@ -1,4 +1,6 @@
 __author__ = 'root'
+
+# TODO: Use pickle to 'pickle' my file.
 # Initializing colorama for Windows machines (gotta think about everyone).
 from colorama import init
 init()
@@ -12,6 +14,10 @@ from colorama import Fore, Back, Style
 
 # Creates an object to store our address books. Module to display all contacts
 class AddressBook(object):
+    # TODO: write docstrings to pep-0257 https://www.python.org/dev/peps/pep-0257/.
+    """Stores a
+
+    """
     def __init__(self, owner):
         self.owner = owner
         self.list_of_contacts = {}
@@ -21,11 +27,11 @@ class AddressBook(object):
             print key
 
     def create_contact(self):
-        self.list_of_contacts [] =
+        # !self.list_of_contacts [] =
         # TODO: create new contact objects from Contact class
 
 
-    def delete_contact(self):
+    #def delete_contact(self):
         """
         select an exiting contact object
         delete the selected contact object
@@ -67,15 +73,19 @@ class Contacts(object):
         """
 
     def edit(self):
-        """
-        show current instance attribute values
-        allow for editing of those values
-        """
+
+        self.name = raw_input(fg + 'Please enter the first name of your contact: ')
+        self.last_name = raw_input(fg + 'Please enter the last name of your contact: ')
+        self.phone = raw_input(fg + "Please enter %s's phone number: " % first_name)
+        self.alt_phone = raw_input(fg + "Please enter an alternative number for %s: " % first_name)
+        self.address = raw_input(fg + "Please enter %s's address: " % first_name)
+        self.email = raw_input(fg + "Please enter %s's email: " % first_name)
+
 
 
 
 def main_menu(address_book_owner):
-
+    # creates shortcuts for colorama that we are using.
     fg = Fore.GREEN
     fr = Fore.RED
     sra = Style.RESET_ALL
@@ -111,14 +121,18 @@ def main_menu(address_book_owner):
                 test = raw_input(Fore.GREEN + 'Press any key to continue: ')
             elif selection == 3:
                 print(Fore.GREEN + 'Create New Contact' + Style.RESET_ALL)
-                first_name = raw_input(fg + 'Please enter the first name of your contact: ')
-                last_name = raw_input(fg + 'Please enter the last name of your contact: ')
+
+                # TODO: create a new key for full_name inside of dictionary gregs_contacts.list_of_contacts
+                # TODO: instantiate new instance of Contact, named full_name.
+                # TODO: give full_name attributes of first_name, last_name,
 
             elif selection == 6:
                 print(Fore.RED + 'Thanks for playing')
                 break
 
 
+# Instantiate an instance of AddressBook named gregs_contacts (note spelling my name wrong was Grant's idea)
+# with attributes
 gregs_contacts = AddressBook('Gregg')
 print gregs_contacts.owner
 
@@ -129,145 +143,3 @@ gregs_contacts.list_of_contacts[test_contact.full_name] = test_contact
 main_menu(gregs_contacts)
 
 
-
-'''
-
-test_contact.display()
-#print(gregs_contacts.list_of_contacts.keys())
-gregs_contacts.display_all_contacts()
-'''
-
-'''
-def sub_menu():
-    print '-' * 10
-
-
-book = {}
-
-menu_selection = 0
-while menu_selection != 5:
-    main_menu()
-    menu_selection = raw_input(">")
-
-    # create contact
-    while menu_selection == "1":
-        print "Create Contact"
-        print "Name:"
-        create_contact = raw_input('>')
-# check if user exists, if yes prompt ' this will overwrite create_contact are you sure (y,n)'
-        print "Phone number:"
-        create_phone = raw_input('>')
-        print "Alternative phone number:"
-        create_altPhone = raw_input('>')
-        print "Address:"
-        create_address = raw_input('>')
-        print "Email:"
-        create_email = raw_input('>')
-        book = {create_contact: {"name": create_contact,
-                                 " phone": create_phone,
-                                 " alt phone": create_altPhone,
-                                 " address": create_address,
-                                 " email": create_email}}
-# reformat print book so that it looks pretty.
-        print book
-        print ""
-        print("Would you like to create another contact? (y,n)")
-        create_prompt = raw_input('>')
-        if create_prompt == 'y':
-            print "\n"
-        else:
-            break
-
-# format menu 2 so that it matches the main menu.
-# list possible variables to edit as a numbered list with each possible variable.
-    while menu_selection == "2":
-        print "Edit Contact"
-        print "Which contact would you like to edit?"
-        edit_contact = raw_input('>')
-        print "Are you sure you want to edit %s? (y,n)" % edit_contact
-        edit_confirm = raw_input('>')
-        if edit_confirm == 'y':
-            print "Which field would you like to edit? (name, address, phone, alt_phone, email)"
-            edit_field = raw_input('>')
-            print "Are you sure you want to edit %s? (y,n)" % edit_field
-            edit_field_confirm = raw_input('>')
-            if edit_field_confirm == 'y':
-                print "What is the new value for %s in contact %s?" % (edit_field, edit_contact)
-                new_edit = raw_input('>')
-                book = {edit_field: new_edit}
-                print book
-                print "Would you like to edit another contact (y,n)"
-                continue_prompt = raw_input('>')
-                if continue_prompt == 'y':
-                    print "\n"
-                else:
-                    break
-
-            else:
-                break
-        else:
-            break
-
-# view contacts
-    while menu_selection == "3":
-        print "View Contact"
-        print "Which contact would you like to see?"
-        contact_prompt = raw_input('>')
-# need a way to check that contact_prompt is valid.
-        print(book[contact_prompt])
-        #print("Name:" book[contact_prompt][create_contact])
-        print "Would you like to view another contact? (y,n)"
-        continue_prompt = raw_input('>')
-        if continue_prompt == 'y':
-            print "\n"
-        else:
-            break
-
-# delete contact menu item
-    while menu_selection == "4":
-        print "Delete Contact"
-        print "Which contact would you like to delete?"
-        delete_prompt = raw_input('>')
-        # check that input is valid
-        print "Are you sure you want to remove %s (y,n)" % delete_prompt
-        delete_confirm = raw_input('>')
-        if delete_confirm == 'y':
-            book.pop(delete_prompt)
-            print book
-            print "Would you like to remove another contact? (y,n)"
-            delete_prompt = raw_input('>')
-            if delete_prompt == 'y':
-                print "\n"
-            else:
-                print "\n"
-                break
-
-        else:
-            print "\n"
-            break
-
-
-# quit menu item
-    while menu_selection == "5":
-        quit_confirm = raw_input("Are you sure you want to quit (y,n)?")
-        if quit_confirm == 'y':
-            print "Thanks for playing!"
-            break
-        elif quit_confirm == 'n':
-            menu_selection = 0
-            break
-
-    if menu_selection == "5":
-        break'''
-
-'''Person.name = raw_input('please enter a name: ')
-print(Person.name)
-Person.phone = raw_input('please enter a phone number: ')
-print(Person.phone)
-Person.alt_phone = raw_input('please enter an alt phone number: ')
-print(Person.alt_phone)
-Person.address = raw_input('please enter an address: ')
-print(Person.address)
-Person.email = raw_input('please enter an email: ')
-print(Person.email)
-'''
